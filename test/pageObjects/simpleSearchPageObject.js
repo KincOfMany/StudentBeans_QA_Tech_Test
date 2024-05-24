9b702c39957b18099ca5c7df3821c5bff6109571
Paul Fitzgerald <trezten@gmail.com>
2024-05-24 14:48:23 +0100

    got it running and first step complete

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,14 +1,33 @@
-const ParentPageObject = require('./parentPageObject')
+const ParentPageObject = require("./parentPageObject");
 
 class SimpleSearchPageObject extends ParentPageObject {
-  async goToHomePage () {
+  async goToHomePage() {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
-    await browser.url('')
+    await browser.url("");
+    if ($("button=Accept All Cookies").isDisplayed()) {
+      await $("button=Accept All Cookies").click();
+    }
   }
 
-  async verifyHomePage () {
-    await this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+  async verifyHomePage() {
+    // This no longer seems to be on the homepage
+    // await this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
+    await this.isElementEqualToExpected(
+      $("h2=Student deals of the day"),
+      "Student deals of the day"
+    );
+  }
+
+  async clickOnSearch() {
+    await $("p=Brands, items or categories").click();
+  }
+
+  async verifySearch() {
+    await this.isElementEqualToExpected(
+      $("h3=Promoted Offers"),
+      "Promoted Offers"
+    );
   }
 }
 
-module.exports = SimpleSearchPageObject
+module.exports = SimpleSearchPageObject;
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,9 +1,23 @@
-const { Given } = require('@cucumber/cucumber')
-const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
+const { Given } = require("@cucumber/cucumber");
+const SimpleSearchPageObject = require("../pageObjects/simpleSearchPageObject");
+const { When, Then } = require("@wdio/cucumber-framework");
 
-const simpleSearchPageObject = new SimpleSearchPageObject()
+const simpleSearchPageObject = new SimpleSearchPageObject();
 
-Given('I am on the studentbeans homepage', async () => {
-  await simpleSearchPageObject.goToHomePage()
-  await simpleSearchPageObject.verifyHomePage()
-})
+Given("I am on the studentbeans homepage", async () => {
+  await simpleSearchPageObject.goToHomePage();
+  await simpleSearchPageObject.verifyHomePage();
+});
+
+Given("I open the search bar", async () => {
+  await simpleSearchPageObject.clickOnSearch();
+  await simpleSearchPageObject.verifySearch();
+});
+
+When('I enter "Samsung"', async () => {
+  return 'I enter "Samsung"';
+});
+
+Then('I should select the 4th "Samsung" search listing', async () => {
+  return 'I should select the 4th "Samsung" search listing';
+});
